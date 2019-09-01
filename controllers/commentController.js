8bbdf14aeeb0f013af5183c5fdcde96b0f9c9986
andysml <andy922200@gmail.com>
2019-09-01 16:18:04 +0100

    add delete comment function for admin

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -8,6 +8,14 @@ let commentController = {
       RestaurantId: req.body.restaurantId,
       UserId: req.user.id
     })
+  },
+  deleteComment: (req, res) => {
+    return Comment.findByPk(req.params.id).then(comment => {
+      comment.destroy()
+        .then(comment => {
+          res.redirect(`/restaurants/${comment.RestaurantId}`)
+        })
+    })
   }
 }
 
