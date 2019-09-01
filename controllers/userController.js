43b61dc7125a2f427b13b5a84f0ef771f3b8ff72
andysml <andy922200@gmail.com>
2019-09-01 19:16:03 +0100

    for reset heroku

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,7 +46,6 @@ let userController = {
   signInPage: (req, res) => {
     return res.render('signin')
   },
-
   signIn: (req, res) => {
     req.flash('success_messages', '成功登入！')
     res.redirect('/restaurants')
