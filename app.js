31edbc1768601dc3a6f3417716e121ac852e3309
andysml <andy922200@gmail.com>
2019-09-04 02:39:18 +0200

    reconstruct router and GET /api/admin/restaurants

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
 app.use('/upload', express.static(__dirname + '/upload'))
 
 // initialize routes settings
-require('./routes')(app, passport)
+require('./routes')(app)
 
 // initialize app listener
 app.listen(port, () => {
